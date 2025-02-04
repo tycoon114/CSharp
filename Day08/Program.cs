1b904f3b7250ed790d92bca59a41d9fa902e3fa6
tycoon114 <sly114@naver.com>
2025-02-04 09:45:01 +0900

    OrderBy 수정
    비효율적 정렬필요없음

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -6,6 +6,7 @@
         {
 
             int[] array52 = new int[52];
+            int[] array8 = new int[8];
             for (int i = 0; i < 52; i++)
             {
                 array52[i] = i+1;
@@ -13,9 +14,17 @@
 
             Random rand = new Random();
 
-            int[] selectedNumbers = array52.OrderBy(x => rand.Next()).Take(8).ToArray();
 
-            Console.WriteLine("선택된 숫자: " + string.Join(", ", selectedNumbers));
+            for (int i = 0; i < 8; i++)
+            {
+                int index = rand.Next(52 - i); // 남은 요소 개수 내에서 랜덤 선택
+                array8[i] = array52[index];
+
+                // 선택된 요소를 배열 끝으로 이동하여 중복 방지 (Fisher-Yates Shuffle 방식)
+                array52[index] = array52[51 - i];
+            }
+
+            Console.WriteLine("선택된 숫자: " + string.Join(", ", array8));
 
 
         }
