9b6dea77b72fa133130c85736d604101f42f3b04
tycoon114 <sly114@naver.com>
2025-02-04 09:36:34 +0900

    random

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int[] array52 = new int[52];
+            for (int i = 0; i < 52; i++)
+            {
+                array52[i] = i+1;
+            }
+
+            Random rand = new Random();
+
+            int[] selectedNumbers = array52.OrderBy(x => rand.Next()).Take(8).ToArray();
+
+            Console.WriteLine("선택된 숫자: " + string.Join(", ", selectedNumbers));
+
+
         }
     }
 }
