89c9dccc591cf8b75d732165c622766773b18e70
tycoon114 <sly114@naver.com>
2025-02-04 11:32:55 +0900

    덱빌딩

---
+++ Day08/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08
+{
+    public  class Class1
+    {
+        public int temp(int a) {
+            Console.Write("aaa");
+
+            return a + 1;
+        }
+
+        static uint A = 1103515245;
+        static uint C = 12345;
+        static uint M = 2147483648;
+
+        static uint seed = 1;
+
+        static uint rand()
+        {
+            seed = (A * seed + C) % M;
+            return seed;
+        }
+
+        public void deckBuild()
+        {
+            int[] deck = new int[52];
+
+            //1 - 13 -> Heart , 1 -> A, 11 -> J, 12 -> Q , 13 -> K
+            //14 - 26 -> Diamond
+            //27 - 39 -> Clover
+            //40 - 52 -> Spade 
+            //Initialize
+            for (int i = 0; i < deck.Length; i++)
+            {
+                deck[i] = i + 1;
+            }
+
+            //Shuffle 
+            Random random = new Random();
+
+            for (int i = 0; i < deck.Length * 10; ++i)
+            {
+                int firstCardIndex = random.Next(0, deck.Length);
+                int secondCardIndex = random.Next(0, deck.Length);
+
+                int temp = deck[firstCardIndex];
+                deck[firstCardIndex] = deck[secondCardIndex];
+                deck[secondCardIndex] = temp;
+            }
+
+            //Print
+            for (int i = 0; i < 8; ++i)
+            {
+
+                int k = deck[i] / 13;
+
+                string teir = checkTier(deck[i]);
+                string temp = "";
+
+                if (k == 0)
+                {
+                    temp = "Heart";
+                }
+                else if (k == 1)
+                {
+                    temp = "Dia";
+                }
+                else if (k == 2)
+                {
+                    temp = "Clov";
+                }
+                else if (k == 3) {
+                    temp = "Spade";
+                }
+
+                Console.WriteLine(temp + "  " + teir);
+            }
+        }
+
+
+        public string checkTier(int numb) {
+
+            if (numb % 13 == 1)
+            {
+                return "ACE";
+            }
+            else if (numb % 13 == 11)
+            {
+                return "J";
+            }
+            else if (numb % 13 == 12)
+            {
+                return "Q";
+            }
+            else if (numb % 13 == 0)
+            {
+                return "K";
+            }
+            else {
+                numb = numb % 13;
+            }
+
+
+            return numb.ToString();
+        }
+
+
+    }
+}
--- Day08/Program.cs
+++ Day08/Program.cs
@@ -7,24 +7,35 @@
 
             int[] array52 = new int[52];
             int[] array8 = new int[8];
-            for (int i = 0; i < 52; i++)
-            {
-                array52[i] = i+1;
-            }
+            //for (int i = 0; i < 52; i++)
+            //{
+            //    array52[i] = i+1;
+            //}
 
-            Random rand = new Random();
+            //Random rand = new Random();
 
 
-            for (int i = 0; i < 8; i++)
-            {
-                int index = rand.Next(52 - i); // 남은 요소 개수 내에서 랜덤 선택
-                array8[i] = array52[index];
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    int index = rand.Next(52 - i); // 남은 요소 개수 내에서 랜덤 선택
+            //    array8[i] = array52[index];
 
-                // 선택된 요소를 배열 끝으로 이동하여 중복 방지 (Fisher-Yates Shuffle 방식)
-                array52[index] = array52[51 - i];
-            }
+            //    // 선택된 요소를 배열 끝으로 이동하여 중복 방지 (Fisher-Yates Shuffle 방식)
+            //    array52[index] = array52[51 - i];
+            //}
 
-            Console.WriteLine("선택된 숫자: " + string.Join(", ", array8));
+            //Console.WriteLine("선택된 숫자: " + string.Join(", ", array8));
+
+            Class1 class1 = new Class1();
+            //int a = class1.temp(12);
+            //Console.WriteLine(a);
+
+
+            class1.deckBuild();
+
+
+
+
 
 
         }
