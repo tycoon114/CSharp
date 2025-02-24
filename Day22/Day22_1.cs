155351263027c9dd0351fc4af17e504f6324f3b6
tycoon114 <sly114@naver.com>
2025-02-24 11:58:57 +0900

    코테 예제 풀이
    비트연산자
    
    제곱 어쩌구 나오면 Q2 같이 비트연산자 '<<' 로 제곱 구현

---
+++ Day22/Day22_1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22
+{
+
+
+    internal class Map
+    {
+        public void Temp()
+        {
+
+            //int n = 5;
+
+            //uint[] arrA = [9, 20, 28, 18, 11];  //[9, 20, 28, 18, 11]
+            //uint[] arrB = [30, 1, 21, 17, 28];  //[30, 1, 21, 17, 28]
+
+            //string[] arrR = new string[n];
+
+            ////string numStr = Convert.ToString(n, 2);
+            //uint k = 0;
+            //for (int i = 0; i < arrA.Length; i++)
+            //{
+            //    k = arrA[i] | arrB[i];
+
+            //    Console.WriteLine(Convert.ToString(k, 2));
+            //}
+
+
+        }
+
+
+        public void Kakao()
+        {
+            int n = 5;
+            int[] arrA = { 9, 20, 28, 18, 11 };
+            int[] arrB = { 30, 1, 21, 17, 28 };
+
+            int[] arrR = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arrR[i] = arrA[i] | arrB[i];
+                //Console.WriteLine(Convert.ToString(arrR[i], 2).Replace('1','#').Replace('0',' '));
+            }
+
+            int bitMask = 0b00000001;
+            for (int i = 0; i < n; ++i)
+            {
+
+                bitMask = 1 << (n - 1);
+                for (int j = 0; j < 8; ++j)
+                {
+                    Console.Write((bitMask & arrR[i]) > 0 ? "#" : " ");
+                    bitMask = bitMask >> 1;
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+
+
+        public void bitQ2()
+        {
+
+            uint N = 3;
+            ulong[] X = new ulong[4];
+
+            X[0] = 3;
+            X[1] = 5;
+            X[2] = 7;
+
+            ulong result = 0;
+
+            for (int i = 0; i < N; ++i)
+            {
+                ulong value = 1;
+                for (int n = 1; n < 64; ++n)
+                {
+                    value = value << 1;
+                    if (X[i] < value)
+                    {
+                        //Console.Write(value);
+                        result = result ^ value;
+
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+
+        }
+
+
+    }
+
+}
+
---
+++ Day22/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Day22
+{
+
+    class BitArray32 {
+        public uint Data;
+
+        public void On(int position) {
+            Data = Data | (uint)(1 << position -1);
+        }
+
+        public void Off(int position)
+        {
+            Data = Data & ~(uint)(1 << position);
+        }
+
+    }
+
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //byte a = 0; // 0000 0000
+            //a = 1 << 1;  // ===> 0000 0010
+            //Console.WriteLine(a);
+            //byte b = 0;
+            //b = 1 << 2; // ====> 0000 0100
+            //Console.WriteLine(b);
+
+
+            //int R = 255;
+            //R = 0xFF;
+            //R = 0b11111111;
+            byte Player = 1; // 0b0000 0001
+            byte Camera = 2; // 0b0000 0010
+            byte UI = 4;     // 0b0000 0100
+            byte water = 8;  // 0b0000 1000
+
+            //Player
+            byte playerLayer = 0b00000000;   // 0b00000000
+            playerLayer = (byte)(playerLayer | Player);
+
+
+            //비트 마스킹 
+            //플레이어나 카메라 레이어 중 최소 1개만 고른다면
+            if ((playerLayer & (Player | Camera))>(byte)0) { 
+            
+            }
+
+
+            BitArray32 bitarray = new BitArray32();
+
+            Map map = new Map();
+            //map.Kakao();
+            map.bitQ2();
+
+        }
+    }
+}
