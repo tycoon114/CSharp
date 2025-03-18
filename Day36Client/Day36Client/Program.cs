4fc722e203e7f82115f1b9e969fea1ebd049bd74
tycoon114 <sly114@naver.com>
2025-03-18 09:21:11 +0900

    서버 통신

---
+++ Day36Client/Day36Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day36Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000);
+            serverSocket.Connect(serverEndPoint);
+
+            byte[] buffer;
+            String message = "안녕하세요";
+            buffer = Encoding.UTF8.GetBytes(message);
+
+            serverSocket.Send(buffer, 0 ,buffer.Length, SocketFlags.None);
+
+            byte[] recieveBuffer = new byte[1048];
+            serverSocket.Receive(recieveBuffer);
+
+            Console.WriteLine(Encoding.UTF8.GetString(recieveBuffer));
+
+            serverSocket.Close();
+        }
+    }
+}
---
+++ Day36Client/Day36Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day36Server
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000);
+
+            listenSocket.Bind(serverEndPoint);
+            listenSocket.Listen(10);
+
+            while (isRunning) { 
+                Socket clientSocket = listenSocket.Accept();
+                byte[] buffer = new byte[1024];
+                int recieveLength = clientSocket.Receive(buffer);
+
+                Console.WriteLine(Encoding.UTF8.GetString(buffer));
+
+                byte[] sendBuffer;
+                if (recieveLength > 0) {
+                    String sendMessage = "반가워요";
+                    sendBuffer = Encoding.UTF8.GetBytes(sendMessage);
+                    clientSocket.Send(sendBuffer);
+                }
+                clientSocket.Close();
+            }
+
+            listenSocket.Close();
+        }
+    }
+}
