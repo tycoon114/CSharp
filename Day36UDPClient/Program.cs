f7766bbb1e37daa65b084aef6399c084ef0c1567
tycoon114 <sly114@naver.com>
2025-03-18 15:34:04 +0900

    UDP

---
+++ Day36UDPClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day36UDPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 6000);
+
+            byte[] buffer = new byte[1024];
+
+            string message = "안녕하세요";
+            buffer = Encoding.UTF8.GetBytes(message);
+            int SendLength = serverSocket.SendTo(buffer, buffer.Length,SocketFlags.None,serverEndPoint);
+
+            byte[] buffer2 = new byte[1024];
+            EndPoint remoteEndPoint = serverEndPoint;
+            int RecvLength = serverSocket.ReceiveFrom(buffer2, ref remoteEndPoint);
+
+
+            string message2 = Encoding.UTF8.GetString(buffer2);
+
+            Console.WriteLine(message2);
+
+            serverSocket.Close();
+        }
+    }
+}
---
+++ Day36UDPServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day36UDPServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 6000);
+
+            serverSocket.Bind(serverEndPoint);
+
+            //이때 보내주는 크기가 1024보다 크면 못받음 -> UDP는 쪼개서 받지 않기 때문
+            byte[] buffer = new byte[1024];
+            EndPoint clientEndpoint = (EndPoint)serverEndPoint;
+            int RecvLength = serverSocket.ReceiveFrom(buffer, ref clientEndpoint);
+
+            int SendLength = serverSocket.SendTo(buffer, clientEndpoint);
+            
+
+
+            //serverSocket.Close();
+        }
+    }
+}
