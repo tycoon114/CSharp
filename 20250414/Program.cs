49bb093fcfa5438ff6b1268e965363ab1692bc88
tycoon114 <sly114@naver.com>
2025-04-14 17:49:23 +0900

    스택 큐

---
+++ 20250414/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20250414
+{
+    internal class Program
+    {
+        static int N;
+        static int K;
+        static int MaxQueueSize;
+
+
+
+        static string inputString;
+        static Queue<long> qu = new Queue<long>();
+
+
+        static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+            MaxQueueSize = N;
+
+
+            for (int i = 1; i <= N; i++)
+            {
+                qu.Enqueue(i);
+}
+
+            while (qu.Count > 1)
+            {
+                qu.Dequeue();
+                qu.Enqueue(qu.Dequeue());
+            }
+            Console.Write(qu.Dequeue());
+
+        }
+    }
+}
+
---
+++ D20250414_1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D20250414_1
+{
+    class Queue
+    {
+        private int[] _container = new int[10004];
+        int _top = -1;
+        int _size = 0;
+        int lastValue;
+
+        public void Push(int x)
+        {
+            _container[++_top] = x;
+            lastValue = x;
+            ++_size;
+        }
+
+        public int Pop()
+        {
+            if (_top == -1)
+            {
+                return -1;
+            }
+
+            --_size;
+            return _container[_top--];
+        }
+
+        public int Size() => _size;
+
+        public int Empty()
+        {
+            return (_size == 0) ? 1 : 0;
+        }
+        public int Front()
+        {
+            if (_size == 0)
+            {
+                return -1;
+            }
+
+            return _container[0];
+        }
+
+        public int Back()
+        {
+            if (_size == 0)
+            {
+                return -1;
+            }
+
+            return lastValue;
+        }
+
+    }
+
+
+    class Program
+    {
+        static int N;
+        
+        static Queue qu = new Queue();    
+
+        static void Main()
+        {
+            N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; ++i)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                switch (command[0])
+                {
+                    case "push":
+                        int operand = int.Parse(command[1]);
+                        qu.Push(operand);
+                        break;
+                    case "pop":
+                        Console.WriteLine(qu.Pop());
+                        break;
+                    case "size":
+                        Console.WriteLine(qu.Size());
+                        break;
+                    case "front":
+                        Console.WriteLine(qu.Front());
+                        break;
+                    case "empty":
+                        Console.WriteLine(qu.Empty());
+                        break;
+                    case "back":
+                        Console.WriteLine(qu.Back());
+                        break;
+                }
+            }
+
+        }
+    }
+}
