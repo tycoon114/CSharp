78e2987337846afa1be8215814ee6d85aaae30e4
tycoon114 <sly114@naver.com>
2025-04-28 17:50:55 +0900

    _3 : A*알고리즘...
    완성도 못했고 이해도 못함

--- D20250428_2/Program.cs
+++ D20250428_2/Program.cs
@@ -4,6 +4,7 @@
     {
         //연결되지 않음
         const int INF = 987654321;
+        const int NoWay = -1;
 
         private static int[][] graph;
 
@@ -23,14 +24,29 @@
         public static void Main(string[] args)
         {
             ConstructGraph();
+            int[] path = null;
+            Console.WriteLine(GetDistance(0, 3, out path));
 
-            Console.WriteLine(GetDistance(0, 3));
+            int current = 3;
+            int parent = path[current];
+
+            while (true)
+            {
+                Console.WriteLine($"{current}  <- {path[current]}");
+                current = path[current];
+
+                if (current == path[current])
+                {
+                    Console.WriteLine($"{current}");
+                    break;
+                }
+            }
         }
 
         //GetDistance : 최단 거리를 구하는 함수
         //입력 : 시작, 도착
         //출력 : 최단 거리
-        public static int GetDistance(int start, int goal)
+        public static int GetDistance(int start, int goal, out int[] path)
         {
             // 1. 시작에서 다른 모든 정점까지의 거리를 저장할 배열 생성
             // ㄴ 1.1 start에서 i 까지의 최단 거리
@@ -41,6 +57,15 @@
                 dist[i] = INF;
             }
             dist[start] = 0;
+
+            //path 배열 생성, 초기화
+            //path[i] : start -> i 가는데 경유하는 정점
+            path = new int[7];
+            for (int i = 0; i < path.Length; i++)
+            {
+                path[i] = NoWay;
+            }
+            path[start] = start;
 
             // 2. 방문하지 않은 정점 중 dist가 최소인 정점을 찾기 위한 우선순위 큐를 생성한다.
             PriorityQueue<int, int> pq = new();
@@ -56,13 +81,13 @@
                 // 3.2 dist 배열 갱신
                 for (int v = 0; v < graph[next].Length; v++)   //연결된 정점만 살펴본다.
                 {
-
                     int disViaNext = dist[next] + graph[next][v];  //start에서 next -> v 로 가는 최단 거리 계산
 
                     //최단거리 비교
                     //ㄴ start -> next -> v가 더 짧다면 갱신하고,pq에 삽입
                     if (disViaNext < dist[v])
                     {
+                        path[v] = next;
                         dist[v] = disViaNext;
                         pq.Enqueue(v, dist[v]);
                     }
---
+++ D20250428_3/Program.cs
@@ -0,0 +1,153 @@
+﻿namespace D20250428_3
+{
+    internal class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            ConstructMap();
+
+            int path;
+            //path[i] : start ->i 가는데 경유한 정점
+            //start : 0
+
+        }
+
+        const int MAX_Y = 10;
+        const int MAX_X = 10;
+        static char[][] map = new char[MAX_Y][];
+
+        // 정점 : 좌표 => X,Y
+        static int startX, startY, endX, endY;
+        //FindStartAndEnd() : 시작 지점과 도착 지점을 찾는 함수
+
+        public static void FindStartAndEnd()
+        {
+            for (int y = 0; y < MAX_X; y++)
+            {
+                for (int x = 0; x < MAX_Y; x++)
+                {
+                    if (map[y][x].Equals("S"))
+                    {
+                        startX = x;
+                        startY = y;
+                    }
+                    else if (map[y][x].Equals("G"))
+                    {
+                        endX = x;
+                        endY = y;
+                    }
+                }
+            }
+        }
+
+
+        // 맵을 구성한다.
+        static void ConstructMap()
+        {
+            map[0] = "          ".ToCharArray();
+            map[1] = "          ".ToCharArray();
+            map[2] = "          ".ToCharArray();
+            map[3] = "    #     ".ToCharArray();
+            map[4] = " S  #  G  ".ToCharArray();
+            map[5] = "    #     ".ToCharArray();
+            map[6] = "          ".ToCharArray();
+            map[7] = "          ".ToCharArray();
+            map[8] = "          ".ToCharArray();
+            map[9] = "          ".ToCharArray();
+        }
+
+
+        static void PrintMap()
+        {
+
+        }
+
+        class AStarNode
+        {
+            public int X;
+            public int Y;
+            public int F;
+            public AStarNode Path;
+        }
+
+        //GetHeuristic : 휴리스틱을 구하는 함수 => 맨해튼 거리
+        // 입력 : 좌표 2개 
+        static public int GetHeuristic(int x1, int y1, int x2, int y2)
+        {
+            int dx = Math.Abs(x1 - x2);
+            int dy = Math.Abs(y1 - y2);
+
+            return dx + dy;
+
+        }
+
+
+        static void SetPath()
+        {
+            //경로 생성
+            AStarNode[,] path = new AStarNode[MAX_Y, MAX_X]; //맵과 대응되는 path 객체
+
+            for (int x = 0; x < MAX_X; x++)
+            {
+                for (int y = 0; y < MAX_Y; y++)
+                {
+                    path[y, x] = new AStarNode() { X = x, Y = y };
+                }
+            }
+
+            //우선순위 큐
+            // 원소 : 노드
+            // 우선순위 : F값
+            PriorityQueue<AStarNode, int> pq = new();
+            pq.Enqueue(path[startY, startX], 0);
+
+            //8방향 탐색
+            int[] dy = { -1, -1, -1, 0, 1, 1, 1, 0 };
+            int[] dx = { -1, 0, 1, 1, 1, 0, -1, -1 };
+            int[] dg = { 14, 10, 14, 10, 14, 10, 14, 10 };
+
+            //경로를 찾을 때까지 반복
+            while (pq.Count > 0)
+            {
+                // 1. 다음에 방문할 정점을 가져온다.
+                AStarNode next = pq.Dequeue();
+                // 2. 8방향으로 탐색 진행
+                for (int i = 0; i < 8; i++)
+                {
+                    int nx = next.X + dx[i];
+                    int ny = next.Y + dy[i];
+
+                    //유효성 검사
+                    //nx : 0 ~  MAX_X, ny : 0 ~ MAX_Y
+                    if (nx < 0 || nx >= MAX_X || ny < 0 || ny > MAX_Y)
+                    {
+                        continue;
+                    }
+                    //이동 가능 한가?
+                    if (map[ny][nx] == '#')
+                    {
+                        continue;
+                    }
+                    // ㄴ 2.1 부분 최단 경로를 찾아 큐에 삽입
+
+                    int f = dg[i] + 10 * GetHeuristic(nx, ny, endX, endY);
+                    AStarNode newNode = path[ny, nx];
+
+                    if (newNode.F > f)
+                    {
+                        newNode.F = f;
+                        newNode.Path = next;
+                        pq.Enqueue(newNode, newNode.F);
+                    }
+                }
+            }
+
+            //맵에 경로 표시
+            AStarNode current = path[endY, endX].Path;
+            while
+        }
+
+    }
+}
