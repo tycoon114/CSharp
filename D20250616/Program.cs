f8269146c977b7f15e99a56a5a0a28bc14981893
tycoon114 <sly114@naver.com>
2025-06-17 17:37:49 +0900

    .

---
+++ D20250616/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace D20250616
+{
+    internal class Program
+    {
+        static int K, N;
+        static int min = int.MaxValue;
+        public static readonly StreamReader input = new(new BufferedStream(Console.OpenStandardInput()));
+        public static readonly StreamWriter output = new(new BufferedStream(Console.OpenStandardOutput()));
+
+        static void Main(string[] args)
+        {
+            int[] inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            K = inputs[0];
+            N = inputs[1];
+
+
+
+
+            output.Write(min);
+            output.Flush();
+        }
+
+
+        static int AnswerDp()
+        {
+            //배열? 숫자 받을것을 DP 돌리기
+            //최소 개수를 구해야됨
+            //모든 경우의 수 중 가장 작은 값
+
+
+            // 불가능할 경우 -1
+
+
+
+            return min;
+        }
+
+    }
+}
---
+++ D20250617/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Text;
+
+namespace D20250617
+{
+    public class Program
+    {
+        static int N, M;
+        static int[] Arr, Arr2;
+
+
+        public static readonly StreamReader input = new(new
+            BufferedStream(Console.OpenStandardInput()));
+        public static readonly StreamWriter output = new(new
+            BufferedStream(Console.OpenStandardOutput()));
+        static void Main(string[] args)
+        {
+            int[] inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            N = inputs[0];
+            M = inputs[1];
+
+            //6개 1개
+            //6a + b = N  a는 묶음으로 사는 개수 b 는 낱개
+            //묶음 중 가장 싼거 찾고 N보다 작은 수중 최댓값을 a
+            // 낱개중 가장 싼거를 찾고 b
+            //해서 둘이 가격 합한거 출력
+
+            for(int i = 0; i < M; i++)
+            {
+                int[] guitar = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+                Arr[i] = guitar[0];
+                Arr2[i] = guitar[1];
+            }
+
+            Array.Sort(Arr);
+            Array.Sort(Arr2);
+
+            output.Flush();
+        }
+    }
+}
